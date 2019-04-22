2fdfdf1eb25065c028ca04cdf295aa7031482339
Maria Camenzuli <maria.camenzuli93@gmail.com>
2019-04-22 16:59:33 +0100

    Removing unused cube model definition

--- ProceduralLevelGenerator/Simulation/ModelLoader.cpp
+++ ProceduralLevelGenerator/Simulation/ModelLoader.cpp
@@ -1,6 +1,5 @@
 #include "ModelLoader.h"
 #include "../3D Components/Models/ObjModel.h"
-#include "../3D Components/Models/CubeModel.h"
 
 ModelLoader::ModelLoader() = default;
 
@@ -13,8 +12,6 @@ Model* ModelLoader::getModel(ModelId modelId) {
     } else {
         const Material cubeMaterial(D3DXVECTOR4(0.4f, 0.6f, 0.4f, 1.0f), D3DXVECTOR4(0.4f, 0.6f, 0.4f, 1.0f), D3DXVECTOR4(0.015532f, 0.005717f, 0.002170f, 1.0f));
         switch (modelId) {
-        case ModelId::CUBE:
-            return modelMap.insert(std::make_pair(modelId, std::make_unique<CubeModel>(CubeModel(cubeMaterial)))).first->second.get();
         case ModelId::HEXAGON:
             return modelMap.insert(std::make_pair(modelId, std::make_unique<ObjModel>(ObjModel::loadFromFile("Resources/Models/blender-hexagon.obj")))).first->second.get();
         default:
--- ProceduralLevelGenerator/Simulation/ModelLoader.h
+++ ProceduralLevelGenerator/Simulation/ModelLoader.h
@@ -11,7 +11,6 @@ public:
     ~ModelLoader();
 
     enum class ModelId {
-        CUBE,
         HEXAGON
     };
 
